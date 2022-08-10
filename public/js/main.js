1a038441f75edf973494c921ed40cf0caa660e93
hromand <hugoromandiyarian@gmail.com>
2022-08-10 07:50:47 -0300

    ws

---
+++ public/js/main.js
@@ -0,0 +1,65 @@
+const socket = io();
+
+const addprod = document.querySelector('#send');
+
+addprod.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const price = document.getElementById("price").value;
+    const link = document.getElementById("link").value;
+
+    const prod = {
+        "title": title,
+        "price": price,
+        "link":link
+    }
+
+    socket.emit("PRODUCT_ADDED",prod)
+    addprod.reset();
+
+})
+
+function sendMessage(){
+    const email = document.getElementById("email").value
+    const message = document.getElementById("message").value
+
+    socket.emit("POST_MESSAGE",{email,message})
+
+    
+    document.getElementById("mensajes").value=""
+   
+}
+
+
+socket.on("NEW_MESSAGE",(msg)=>{
+    appendMessage(msg);
+})
+
+function appendMessage(msg){
+ 
+    const newMessage=`<div class="lineMessage"><p style="color:#405EF7">${msg.email}</p><p style="color:#906840">[${msg.dateTime}]: </p><p style="color:#71EB7A">${msg.message}</p><div>`
+    document.getElementById("messages").innerHTML+=newMessage;
+}
+
+
+socket.on("INIT",(messages)=>{
+
+    for(let msg of messages){
+     
+         appendMessage(msg)
+    }
+ 
+})
+
+
+socket.on("PRODUCTS", async (response) => {
+    const url = "http://localhost:8080/tabla.hbs";
+      fetch(url).then((resp) => {
+        return resp.text();
+    }).then((text) => {
+      const template = Handlebars.compile(text);
+      const html = template({response});
+      document.querySelector("#items").innerHTML = html;
+    });
+  })
--- server.js
+++ server.js
@@ -1,49 +1,52 @@
 const express = require("express");
 const app = express();
-const handlebars = require("express-handlebars");
 
-const hbs = handlebars.create(
-    {
-        extname: ".hbs",
-        defaultLayout: "index.hbs",
-        layoutsDir: __dirname + "/views/layout",
-    }
-)
+const {Server : SocketServer} = require('socket.io');
+const {Server : HTTPServer} = require('http');
+
+const httpServer = new HTTPServer(app);
+const socketServer = new SocketServer(httpServer);
 
 app.use(express.static("public"));
 
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
-app.set("views", "./views");
-
 const Contenedor = require("./utils/contenedor");
-const contenedor1 = new Contenedor("productos.json", "utf-8");
+const managerProductos = new Contenedor("productos.json", "utf-8");
+const managerMensajes = new Contenedor("mensajes.json", "utf-8");
+
+const messages = [];
 
 const PORT = 8080;
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.render("formulario")
+    res.sendFile(__dirname + '/public/index.html');
 });
 
-// GET "/api/productos" -> devuelve todos los productos
-app.get("/api/productos", (req, res) => {
-    let comp = contenedor1.getAll();
-    console.log(comp.productos)
-    if(comp.productos[0]==null){
-        res.send(`<h1>No hay productos </h1>`)
-    } else{
-        res.render("tabla", contenedor1.getAll() );
-    }
-});
-
-//POST "/api/productos" -> agrega un producto 
-app.post("/api/productos", (req, res) => {
-    contenedor1.save(req.body);
-    res.render("tabla", contenedor1.getAll() );
-});
+socketServer.on("connection", (socket)=>{
+    
+    socket.emit("INIT", managerMensajes.getAll());
+  
+  
+    socket.emit("PRODUCTS", managerProductos.getAll())
+   
+    socket.on("PRODUCT_ADDED",(obj)=>{
+        managerProductos.save(obj)
+        socketServer.sockets.emit("PRODUCTS", managerProductos.getAll())
+    })
+  
+    socket.on("POST_MESSAGE", (msg)=>{
+        const dateTime = new Date();
+        const fecha = dateTime.getDate() + '-' + ( dateTime.getMonth() + 1 ) + '-' + dateTime.getFullYear();
+        const hora = dateTime.getHours() + ':' + dateTime.getMinutes() + ':' + dateTime.getSeconds();
+        msg.dateTime=fecha+" "+hora;
+        messages.push(msg);
+        managerMensajes.save(msg);
+        socketServer.sockets.emit("NEW_MESSAGE",msg)
+    })
+  
+})
 
 app.listen(PORT, () => {
    console.log(`Servidor escuchando en puerto ${PORT}`);
