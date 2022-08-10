0ae154c7ba06e3228e1b27764f87c9d4dab52305
hromand <hugoromandiyarian@gmail.com>
2022-08-09 23:49:17 -0300

    agregando chat

--- README-opinion.md
+++
@@ -1,5 +0,0 @@
-# Motores de plantilla
-
-Después de trabajar con estos tres tipos de plantillas, la elección mas practica me parece PUG. Me resulto mas practica, fácil de implementar y encontre mas documentacion accesible. 
-
-Hugo.
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const ejs = require("ejs")
 const app = express();
+const handlebars = require("express-handlebars");
 
-app.set("view engine", "ejs");
+const hbs = handlebars.create(
+    {
+        extname: ".hbs",
+        defaultLayout: "index.hbs",
+        layoutsDir: __dirname + "/views/layout",
+    }
+)
+
+app.use(express.static("public"));
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./views");
 
 const Contenedor = require("./utils/contenedor");
 const contenedor1 = new Contenedor("productos.json", "utf-8");
@@ -13,19 +25,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.render("page/formulario")
+    res.render("formulario")
 });
 
 // GET "/api/productos" -> devuelve todos los productos
 app.get("/api/productos", (req, res) => {
-    res.render("page/tabla", contenedor1.getAll() );
+    let comp = contenedor1.getAll();
+    console.log(comp.productos)
+    if(comp.productos[0]==null){
+        res.send(`<h1>No hay productos </h1>`)
+    } else{
+        res.render("tabla", contenedor1.getAll() );
+    }
 });
 
-//POST "/api/productos" -> agrega un producto
- app.post("/api/productos", (req, res) => {
+//POST "/api/productos" -> agrega un producto 
+app.post("/api/productos", (req, res) => {
     contenedor1.save(req.body);
-    res.render("page/tabla", contenedor1.getAll() );
- });
+    res.render("tabla", contenedor1.getAll() );
+});
 
 app.listen(PORT, () => {
    console.log(`Servidor escuchando en puerto ${PORT}`);
